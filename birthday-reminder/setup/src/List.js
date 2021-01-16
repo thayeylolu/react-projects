efb1686a9dca2fe56f3939896520882ac688adab
thayeylolu <hamdallahhassanah@gmail.com>
2021-01-16 15:37:02 +0100

    updated birthday app

--- birthday-reminder/setup/src/List.js
+++ birthday-reminder/setup/src/List.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from 'react'
 
-const List = () => {
+//people is a prop here
+const List = ({ people }) => {
   return (
     <>
-      <h2>list component</h2>
+      {people.map((person) => {
+        const { id, name, image, age } = person
+        return (
+          <article key={id} className='person'>
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        )
+      })}
     </>
-  );
-};
+  )
+}
 
-export default List;
+export default List
