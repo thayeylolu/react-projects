41d24677553d5cfead07d9c60cbef292337bc0b8
thayeylolu <hamdallahhassanah@gmail.com>
2021-01-19 22:55:09 +0100

    the button is not responsive

--- birthday-reminder/setup/src/Individuals.js
+++ birthday-reminder/setup/src/Individuals.js
@@ -8,19 +8,14 @@ const Individuals = () => {
 		let newPeople = individuals.filter((person) => person.id !== id)
 		setIndividuals(newPeople)
   }
-
+    const { id, name } = individuals
   return (
     <>
-      {Individuals.map((person) => {
-        const { id, name } = person;
-        return (
           <div key={id} className='item'>
             <h4>{name}</h4>
             <button onClick={() => removeItem(id)}>remove</button>
           </div>
-        )
-      })
-}
+
 </>
 )};
 export default Individuals
